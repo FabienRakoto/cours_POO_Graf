e65b055a54f5d8300df6ccb7a960888f0ef0908f
tuyetrinhvo <tuyetrinhvo@gmail.com>
2018-11-10 19:22:53 +0100

    tests PDO

--- TP/pages/home.php
+++ TP/pages/home.php
@@ -1,3 +1,11 @@
-<h1>Home</h1>
-<p class="lead"><a href="index.php?p=single">Single</a></p>
+<?php
 
+$pdo = new PDO('mysql:dbname=blog;host=localhost', 'root', '');
+/*$count = $pdo->exec('INSERT INTO articles SET titre="Mon titre", date="'.date('Y-m-d H:i:s') .'"');
+var_dump($count); die;*/
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$response = $pdo->query('SELECT * FROM articles');
+//var_dump($response->fetchAll(PDO::FETCH_OBJ));
+$data = $response->fetchAll(PDO::FETCH_OBJ);
+var_dump($data[0]->titre);
